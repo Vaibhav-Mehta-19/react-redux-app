7e49eca53332b18189ca637419768d63181fd1c2
Vaibhav Mehta <52502293+Vaibhav-Mehta-19@users.noreply.github.com>
2021-05-13 17:14:40 +0530

    Delete root directory

--- root/Actions.js
+++
@@ -1,5 +0,0 @@
-import {CHANGE_SEARCH_FIELD} from './constants.js';
-export const setSearchField = (text) => ({
-	type: CHANGE_SEARCH_FIELD,
-	payload: text
-})
--- root/Scroll.js
+++
@@ -1,8 +0,0 @@
-const Scroll = (props) =>{
-	return(
-		<div style={{overflowY : 'scroll', border:'5px solid black' , height:'600px'}}>
-		{props.children}
-		</div>
-	)
-}
-export default Scroll;
